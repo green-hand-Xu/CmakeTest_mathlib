d13ddd8512cea09dd78abe2b98721f4ff632e488
许营博 <837085581@qq.com>
2024-05-13 17:14:32 +0800

    1

---
+++ MyMath.cpp
@@ -0,0 +1,19 @@
+#include <MyMath.h>
+
+
+double Add(double a, double b){
+    return a+b;
+}
+
+double sub(double a, double b){
+    return a-b;
+}
+
+double multi(double a, double b){
+    return a*b;
+}
+
+double division(double a, double b){
+    return a/b;
+}
+
---
+++ MyMath.h
@@ -0,0 +1,12 @@
+#ifndef __MYMATH__
+#define __MYMATH__
+
+double Add(double a, double b);
+
+double sub(double a, double b);
+
+double multi(double a, double b);
+
+double division(double a, double b);
+
+#endif // __MYMATH__
